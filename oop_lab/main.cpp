7246bb36ef596ddf0b9aa183ed02c05af8e77e73
ElizabethMegel <rodion.shyshkin@teamdev.com>
2021-06-29 14:45:31 +0300

    Initial Commit

---
+++ oop_lab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  oop_lab
+//
+//  Created by Rodion Shyshkin on 29.06.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
